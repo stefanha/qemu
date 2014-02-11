e3a6f49eeb7a469a6cd61fc6261c9ad1342a6c74
Fam Zheng <famz@redhat.com>
2014-02-11 11:28:38 +0800

    vmdk: Implement .bdrv_get_info()
    This will return cluster_size and is_compressed to caller, if all the
    extents has the same value (or there's only one extent). Otherwise
    return -ENOTSUP.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block/vmdk.c
+++ block/vmdk.c
@@ -2075,6 +2075,24 @@ static ImageInfoSpecific *vmdk_get_specific_info(BlockDriverState *bs)
     return spec_info;
 }
 
+static int vmdk_get_info(BlockDriverState *bs, BlockDriverInfo *bdi)
+{
+    int i;
+    BDRVVmdkState *s = bs->opaque;
+    assert(s->num_extents);
+    bdi->is_compressed = s->extents[0].compressed;
+    bdi->cluster_size = s->extents[0].cluster_sectors << BDRV_SECTOR_BITS;
+    /* See if we have multiple extents but they have different cases */
+    for (i = 1; i < s->num_extents; i++) {
+        if (bdi->is_compressed != s->extents[i].compressed ||
+            bdi->cluster_size !=
+                s->extents[i].cluster_sectors << BDRV_SECTOR_BITS) {
+            return -ENOTSUP;
+        }
+    }
+    return 0;
+}
+
 static QEMUOptionParameter vmdk_create_options[] = {
     {
         .name = BLOCK_OPT_SIZE,
@@ -2131,6 +2149,7 @@ static BlockDriver bdrv_vmdk = {
     .bdrv_has_zero_init           = vmdk_has_zero_init,
     .bdrv_get_specific_info       = vmdk_get_specific_info,
     .bdrv_refresh_limits          = vmdk_refresh_limits,
+    .bdrv_get_info                = vmdk_get_info,
 
     .create_options               = vmdk_create_options,
 };
