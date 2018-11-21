d19117b3a2440b46ee82d605ab47f27ab2fa3019
Dr. David Alan Gilbert <dgilbert@redhat.com>
2018-11-21 18:02:07 +0000

    virtiofsd: Handle reinit
    Allow init->destroy->init  for mount->umount->mount
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- contrib/virtiofsd/fuse_lowlevel.c
+++ contrib/virtiofsd/fuse_lowlevel.c
@@ -1998,6 +1998,7 @@ static void do_init(fuse_req_t req, fuse_ino_t nodeid,
 		se->conn.max_write = bufsize - FUSE_BUFFER_HEADER_SIZE;
 
 	se->got_init = 1;
+	se->got_destroy = 0;
 	if (se->op.init)
 		se->op.init(se->userdata, &se->conn);
 
@@ -2093,6 +2094,7 @@ static void do_destroy(fuse_req_t req, fuse_ino_t nodeid,
 	(void) iter;
 
 	se->got_destroy = 1;
+	se->got_init = 0;
 	if (se->op.destroy)
 		se->op.destroy(se->userdata);
 
