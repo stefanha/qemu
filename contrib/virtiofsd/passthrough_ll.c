6c97c364e8309e943e47ad5002f2ccc6f9b77f42
Stefan Hajnoczi <stefanha@redhat.com>
2019-03-13 09:32:51 +0000

    virtiofsd: add seccomp whitelist
    Only allow system calls that are needed by virtiofsd.  All other system
    calls cause SIGSYS to be directed at the thread and the process will
    coredump.
    
    Restricting system calls reduces the kernel attack surface and limits
    what the process can do when compromised.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    with additional entries by:
    Signed-off-by: Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>
    Signed-off-by: Masayoshi Mizuma <m.mizuma@jp.fujitsu.com>
    Signed-off-by: Misono Tomohiro <misono.tomohiro@jp.fujitsu.com>
    Signed-off-by: piaojun <piaojun@huawei.com>
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Eric Ren <renzhen@linux.alibaba.com>
    ---
    v2:
     * Use SCMP_ACT_TRAP instead of SCMP_ACT_KILL to terminate the entire
       process [Daniel Berrangé]

--- contrib/virtiofsd/passthrough_ll.c
+++ contrib/virtiofsd/passthrough_ll.c
@@ -57,6 +57,7 @@
 #include <sys/wait.h>
 
 #include "passthrough_helpers.h"
+#include "seccomp.h"
 
 #define HAVE_POSIX_FALLOCATE 1
 
@@ -1969,6 +1970,7 @@ static void setup_sandbox(struct lo_data *lo, struct fuse_session *se)
 {
 	setup_namespaces(lo, se);
 	setup_mounts(lo->source);
+	setup_seccomp();
 }
 
 int main(int argc, char *argv[])
