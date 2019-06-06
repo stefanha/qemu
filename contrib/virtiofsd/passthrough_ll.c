53f962f13fde4bfeda42eb9186d6673667b7d48c
Liu Bo <bo.liu@linux.alibaba.com>
2019-06-07 05:43:56 +0800

    virtiofsd: add helper for lo_data cleanup
    This offers an helper function for lo_data's cleanup.
    
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>

--- contrib/virtiofsd/passthrough_ll.c
+++ contrib/virtiofsd/passthrough_ll.c
@@ -2451,6 +2451,23 @@ static gboolean lo_key_equal(gconstpointer a, gconstpointer b)
                la->dev == lb->dev;
 }
 
+static void fuse_lo_data_cleanup(struct lo_data *lo)
+{
+        if (lo->inodes)
+                g_hash_table_destroy(lo->inodes);
+	lo_map_destroy(&lo->fd_map);
+	lo_map_destroy(&lo->dirp_map);
+	lo_map_destroy(&lo->ino_map);
+
+	if (lo->proc_self_fd >= 0) {
+		close(lo->proc_self_fd);
+	}
+
+	if (lo->root.fd >= 0)
+		close(lo->root.fd);
+
+        free((char *)lo->source);
+}
 
 int main(int argc, char *argv[])
 {
@@ -2594,20 +2611,7 @@ err_out2:
 err_out1:
 	fuse_opt_free_args(&args);
 
-        if (lo.inodes)
-                g_hash_table_destroy(lo.inodes);
-	lo_map_destroy(&lo.fd_map);
-	lo_map_destroy(&lo.dirp_map);
-	lo_map_destroy(&lo.ino_map);
-
-	if (lo.proc_self_fd >= 0) {
-		close(lo.proc_self_fd);
-	}
-
-	if (lo.root.fd >= 0)
-		close(lo.root.fd);
-
-        free((char *)lo.source);
+        fuse_lo_data_cleanup(&lo);
 
 	return ret ? 1 : 0;
 }
