ae1f053dff943b4622787c362cdb4fcede38cb47
Stefan Hajnoczi <stefanha@redhat.com>
2019-06-25 16:46:04 +0100

    virtiofsd: add --socket-path=PATH alias for -o vhost_user_socket=PATH
    The latest vhost-user specification documents that device backends
    should accept a --socket-path=PATH command-line option.
    
    Add an alias for -o vhost_user_socket=PATH.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- contrib/virtiofsd/fuse_lowlevel.c
+++ contrib/virtiofsd/fuse_lowlevel.c
@@ -2550,6 +2550,7 @@ static const struct fuse_opt fuse_ll_opts[] = {
 	LL_OPTION("-d", debug, 1),
 	LL_OPTION("--debug", debug, 1),
 	LL_OPTION("allow_root", deny_others, 1),
+	LL_OPTION("--socket-path=%s", vu_socket_path, 0),
         LL_OPTION("vhost_user_socket=%s", vu_socket_path, 0),
 	FUSE_OPT_END
 };
@@ -2567,6 +2568,7 @@ void fuse_lowlevel_help(void)
 	printf(
 "    -o allow_other             allow access by all users\n"
 "    -o allow_root              allow access by root\n"
+"    --socket-path=PATH         path for the vhost-user socket\n"
 "    -o vhost_user_socket=PATH  path for the vhost-user socket\n"
 "    -o auto_unmount            auto unmount on process termination\n");
 }
