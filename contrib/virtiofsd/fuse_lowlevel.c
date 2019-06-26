23e0a60a00588b9ff7cfe0b15006e067472abd38
Stefan Hajnoczi <stefanha@redhat.com>
2019-06-26 17:51:32 +0100

    virtiofsd: use fuse_lowlevel_is_virtio() in fuse_session_destroy()
    vu_socket_path is NULL when --fd=FDNUM was used.  Use
    fuse_lowlevel_is_virtio() instead.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
      pull request 10

--- contrib/virtiofsd/fuse_lowlevel.c
+++ contrib/virtiofsd/fuse_lowlevel.c
@@ -2496,11 +2496,12 @@ void fuse_session_destroy(struct fuse_session *se)
 	if (se->fd != -1)
 		close(se->fd);
 
-        if (se->vu_socket_path) {
-                virtio_session_close(se);
-                free(se->vu_socket_path);
-                se->vu_socket_path = NULL;
-        }
+	if (fuse_lowlevel_is_virtio(se)) {
+		virtio_session_close(se);
+	}
+
+	free(se->vu_socket_path);
+	se->vu_socket_path = NULL;
 
 	free(se);
 }
