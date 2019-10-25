nabla-containers/qemu-virtiofs

virtiofsd: Perform an unmap on destroy
Force unmap all remaining dax cache entries on a destroy.

Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

contrib/virtiofsd/passthrough_ll.c
@@ -1331,6 +1331,20 @@ static void lo_removemapping(fuse_req_t req, struct fuse_session *se,
 	fuse_reply_err(req, ret);
 }
 
+static void lo_destroy(void *userdata, struct fuse_session *se)
+{
+        if (fuse_lowlevel_is_virtio(se)) {
+                VhostUserFSSlaveMsg msg = { 0 };
+
+                msg.len[0] = ~(uint64_t)0; /* Special: means 'all' */
+        	msg.c_offset[0] = 0;
+                if (fuse_virtio_unmap(se, &msg)) {
+                        fprintf(stderr, "%s: unmap during destroy failed\n", __func__);
+                }
+        }
+}
+
+
 static struct fuse_lowlevel_ops lo_oper = {
 	.init		= lo_init,
 	.lookup		= lo_lookup,
@@ -1368,6 +1382,7 @@ static struct fuse_lowlevel_ops lo_oper = {
 #ifdef HAVE_COPY_FILE_RANGE
 	.copy_file_range = lo_copy_file_range,
 #endif
+	.destroy        = lo_destroy,
         .setupmapping   = lo_setupmapping,
         .removemapping  = lo_removemapping,
 };

stefanha/qemu

contrib/virtiofsd/passthrough_ll.c
@@ -1333,6 +1333,20 @@ static void lo_removemapping(fuse_req_t req, struct fuse_session *se,
 	fuse_reply_err(req, ret);
 }
 
+static void lo_destroy(void *userdata, struct fuse_session *se)
+{
+        if (fuse_lowlevel_is_virtio(se)) {
+                VhostUserFSSlaveMsg msg = { 0 };
+
+                msg.len[0] = ~(uint64_t)0; /* Special: means 'all' */
+        	msg.c_offset[0] = 0;
+                if (fuse_virtio_unmap(se, &msg)) {
+                        fuse_log(FUSE_LOG_ERR, "%s: unmap during destroy failed\n", __func__);
+                }
+        }
+}
+
+
 static struct fuse_lowlevel_ops lo_oper = {
 	.init		= lo_init,
 	.lookup		= lo_lookup,
@@ -1370,6 +1384,7 @@ static struct fuse_lowlevel_ops lo_oper = {
 #ifdef HAVE_COPY_FILE_RANGE
 	.copy_file_range = lo_copy_file_range,
 #endif
+	.destroy        = lo_destroy,
         .setupmapping   = lo_setupmapping,
         .removemapping  = lo_removemapping,
 };