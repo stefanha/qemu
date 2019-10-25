nabla-containers/qemu-virtiofs

virtiofsd: Plumb fuse_bufvec through do_write_buf
do_write_buf now gets the bufv, and in the case where there's more than
one element in it (i.e. separate headers and data) ends up passing
it through mostly unmodified (except skipping the header) to
op.write_buf()

Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

contrib/virtiofsd/fuse_lowlevel.c
@@ -1027,11 +1027,12 @@ static void do_write(fuse_req_t req, fuse_ino_t nodeid, const void *inarg)
 }
 
 static void do_write_buf(fuse_req_t req, fuse_ino_t nodeid, const void *inarg,
-			 const struct fuse_buf *ibuf)
+			 struct fuse_bufvec *ibufv)
 {
 	struct fuse_session *se = req->se;
-	struct fuse_bufvec bufv = {
-		.buf[0] = *ibuf,
+        struct fuse_bufvec *pbufv = ibufv;
+	struct fuse_bufvec tmpbufv = {
+		.buf[0] = ibufv->buf[0],
 		.count = 1,
 	};
 	struct fuse_write_in *arg = (struct fuse_write_in *) inarg;
@@ -1041,28 +1042,35 @@ static void do_write_buf(fuse_req_t req, fuse_ino_t nodeid, const void *inarg,
 	fi.fh = arg->fh;
 	fi.writepage = arg->write_flags & 1;
 
-	if (se->conn.proto_minor < 9) {
-		bufv.buf[0].mem = ((char *) arg) + FUSE_COMPAT_WRITE_IN_SIZE;
-		bufv.buf[0].size -= sizeof(struct fuse_in_header) +
-			FUSE_COMPAT_WRITE_IN_SIZE;
-		assert(!(bufv.buf[0].flags & FUSE_BUF_IS_FD));
-	} else {
-		fi.lock_owner = arg->lock_owner;
-		fi.flags = arg->flags;
-		if (!(bufv.buf[0].flags & FUSE_BUF_IS_FD))
-			bufv.buf[0].mem = PARAM(arg);
+        if (ibufv->count == 1) {
+        	if (se->conn.proto_minor < 9) {
+        		tmpbufv.buf[0].mem = ((char *) arg) + FUSE_COMPAT_WRITE_IN_SIZE;
+        		tmpbufv.buf[0].size -= sizeof(struct fuse_in_header) +
+        			FUSE_COMPAT_WRITE_IN_SIZE;
+        		assert(!(tmpbufv.buf[0].flags & FUSE_BUF_IS_FD));
+        	} else {
+        		fi.lock_owner = arg->lock_owner;
+        		fi.flags = arg->flags;
+        		if (!(tmpbufv.buf[0].flags & FUSE_BUF_IS_FD))
+        			tmpbufv.buf[0].mem = PARAM(arg);
 
-		bufv.buf[0].size -= sizeof(struct fuse_in_header) +
-			sizeof(struct fuse_write_in);
-	}
-	if (bufv.buf[0].size < arg->size) {
-		fprintf(stderr, "fuse: do_write_buf: buffer size too small\n");
-		fuse_reply_err(req, EIO);
-		return;
-	}
-	bufv.buf[0].size = arg->size;
+        		tmpbufv.buf[0].size -= sizeof(struct fuse_in_header) +
+        			sizeof(struct fuse_write_in);
+        	}
+        	if (tmpbufv.buf[0].size < arg->size) {
+        		fprintf(stderr, "fuse: do_write_buf: buffer size too small\n");
+        		fuse_reply_err(req, EIO);
+        		return;
+        	}
+        	tmpbufv.buf[0].size = arg->size;
+                pbufv = &tmpbufv;
+        } else {
+                // Input bufv contains the headers in the first element
+                // and the data in the rest, we need to skip that first element
+                ibufv->buf[0].size = 0;
+        }
 
-	se->op.write_buf(req, nodeid, &bufv, arg->offset, &fi);
+	se->op.write_buf(req, nodeid, pbufv, arg->offset, &fi);
 }
 
 static void do_flush(fuse_req_t req, fuse_ino_t nodeid, const void *inarg)
@@ -2174,7 +2182,7 @@ void fuse_session_process_buf_int(struct fuse_session *se,
 
 	inarg = (void *) &in[1];
 	if (in->opcode == FUSE_WRITE && se->op.write_buf)
-		do_write_buf(req, in->nodeid, inarg, buf);
+		do_write_buf(req, in->nodeid, inarg, bufv);
 	else
 		fuse_ll_ops[in->opcode].func(req, in->nodeid, inarg);
 

stefanha/qemu

contrib/virtiofsd/fuse_lowlevel.c
@@ -1010,11 +1010,12 @@ static void do_write(fuse_req_t req, fuse_ino_t nodeid, const void *inarg)
 }
 
 static void do_write_buf(fuse_req_t req, fuse_ino_t nodeid, const void *inarg,
-			 const struct fuse_buf *ibuf)
+			 struct fuse_bufvec *ibufv)
 {
 	struct fuse_session *se = req->se;
-	struct fuse_bufvec bufv = {
-		.buf[0] = *ibuf,
+        struct fuse_bufvec *pbufv = ibufv;
+	struct fuse_bufvec tmpbufv = {
+		.buf[0] = ibufv->buf[0],
 		.count = 1,
 	};
 	struct fuse_write_in *arg = (struct fuse_write_in *) inarg;
@@ -1024,28 +1025,35 @@ static void do_write_buf(fuse_req_t req, fuse_ino_t nodeid, const void *inarg,
 	fi.fh = arg->fh;
 	fi.writepage = arg->write_flags & FUSE_WRITE_CACHE;
 
-	if (se->conn.proto_minor < 9) {
-		bufv.buf[0].mem = ((char *) arg) + FUSE_COMPAT_WRITE_IN_SIZE;
-		bufv.buf[0].size -= sizeof(struct fuse_in_header) +
-			FUSE_COMPAT_WRITE_IN_SIZE;
-		assert(!(bufv.buf[0].flags & FUSE_BUF_IS_FD));
-	} else {
-		fi.lock_owner = arg->lock_owner;
-		fi.flags = arg->flags;
-		if (!(bufv.buf[0].flags & FUSE_BUF_IS_FD))
-			bufv.buf[0].mem = PARAM(arg);
+        if (ibufv->count == 1) {
+        	if (se->conn.proto_minor < 9) {
+        		tmpbufv.buf[0].mem = ((char *) arg) + FUSE_COMPAT_WRITE_IN_SIZE;
+        		tmpbufv.buf[0].size -= sizeof(struct fuse_in_header) +
+        			FUSE_COMPAT_WRITE_IN_SIZE;
+        		assert(!(tmpbufv.buf[0].flags & FUSE_BUF_IS_FD));
+        	} else {
+        		fi.lock_owner = arg->lock_owner;
+        		fi.flags = arg->flags;
+        		if (!(tmpbufv.buf[0].flags & FUSE_BUF_IS_FD))
+        			tmpbufv.buf[0].mem = PARAM(arg);
 
-		bufv.buf[0].size -= sizeof(struct fuse_in_header) +
-			sizeof(struct fuse_write_in);
-	}
-	if (bufv.buf[0].size < arg->size) {
-		fuse_log(FUSE_LOG_ERR, "fuse: do_write_buf: buffer size too small\n");
-		fuse_reply_err(req, EIO);
-		return;
-	}
-	bufv.buf[0].size = arg->size;
+        		tmpbufv.buf[0].size -= sizeof(struct fuse_in_header) +
+        			sizeof(struct fuse_write_in);
+        	}
+        	if (tmpbufv.buf[0].size < arg->size) {
+        		fuse_log(FUSE_LOG_ERR, "fuse: do_write_buf: buffer size too small\n");
+        		fuse_reply_err(req, EIO);
+        		return;
+        	}
+        	tmpbufv.buf[0].size = arg->size;
+                pbufv = &tmpbufv;
+        } else {
+                // Input bufv contains the headers in the first element
+                // and the data in the rest, we need to skip that first element
+                ibufv->buf[0].size = 0;
+        }
 
-	se->op.write_buf(req, nodeid, &bufv, arg->offset, &fi);
+	se->op.write_buf(req, nodeid, pbufv, arg->offset, &fi);
 }
 
 static void do_flush(fuse_req_t req, fuse_ino_t nodeid, const void *inarg)
@@ -2173,7 +2181,7 @@ void fuse_session_process_buf_int(struct fuse_session *se,
 
 	inarg = (void *) &in[1];
 	if (in->opcode == FUSE_WRITE && se->op.write_buf)
-		do_write_buf(req, in->nodeid, inarg, buf);
+		do_write_buf(req, in->nodeid, inarg, bufv);
 	else
 		fuse_ll_ops[in->opcode].func(req, in->nodeid, inarg);
 