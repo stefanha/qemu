nabla-containers/qemu-virtiofs

virtiofsd: Make fsync work even if only inode is passed in
If caller has not sent file handle in request, then using inode, retrieve
the fd opened using O_PATH and use that to open file again and issue
fsync. This will be needed when dax_flush() calls fsync. At that time
we only have inode information (and not file).

Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

contrib/virtiofsd/passthrough_ll.c
@@ -953,10 +953,31 @@ static void lo_fsync(fuse_req_t req, fuse_ino_t ino, int datasync,
 {
 	int res;
 	(void) ino;
+	int fd;
+	char *buf;
+
+	if (lo_debug(req))
+		fprintf(stderr, "lo_fsync(ino=%" PRIu64 ", fi=0x%p)\n", ino,
+				(void *)fi);
+
+	if (!fi) {
+		res = asprintf(&buf, "/proc/self/fd/%i", lo_fd(req, ino));
+		if (res == -1)
+			return (void) fuse_reply_err(req, errno);
+
+		fd = open(buf, O_RDWR);
+		free(buf);
+		if (fd == -1)
+			return (void) fuse_reply_err(req, errno);
+	} else
+		fd = fi->fh;
+
 	if (datasync)
-		res = fdatasync(fi->fh);
+		res = fdatasync(fd);
 	else
-		res = fsync(fi->fh);
+		res = fsync(fd);
+	if (!fi)
+		close(fd);
 	fuse_reply_err(req, res == -1 ? errno : 0);
 }
 

stefanha/qemu

contrib/virtiofsd/passthrough_ll.c
@@ -863,10 +863,30 @@ static void lo_fsync(fuse_req_t req, fuse_ino_t ino, int datasync,
 {
 	int res;
 	(void) ino;
+	int fd;
+	char *buf;
+
+	fuse_log(FUSE_LOG_DEBUG, "lo_fsync(ino=%" PRIu64 ", fi=0x%p)\n", ino,
+		 (void *)fi);
+
+	if (!fi) {
+		res = asprintf(&buf, "/proc/self/fd/%i", lo_fd(req, ino));
+		if (res == -1)
+			return (void) fuse_reply_err(req, errno);
+
+		fd = open(buf, O_RDWR);
+		free(buf);
+		if (fd == -1)
+			return (void) fuse_reply_err(req, errno);
+	} else
+		fd = fi->fh;
+
 	if (datasync)
-		res = fdatasync(fi->fh);
+		res = fdatasync(fd);
 	else
-		res = fsync(fi->fh);
+		res = fsync(fd);
+	if (!fi)
+		close(fd);
 	fuse_reply_err(req, res == -1 ? errno : 0);
 }
 